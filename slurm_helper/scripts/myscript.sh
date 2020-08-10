1848baab85eb69d2e2a7b1bb1d0bb7c2b4136c10
danielbenhayoun <danielben@rishon.csf.technion.ac.il>
2020-08-10 12:23:05 +0300

    added script for training model with slurm

--- slurm_helper/scripts/myscript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Setup env
-source $HOME/miniconda3/etc/profile.d/conda.sh
-conda activate cs236781-hw
-echo "hello from $(python --version) in $(which python)"
-
-# Run some arbitrary python
-python -c 'import torch; print(f"i can haz gpu? {torch.cuda.is_available()}")'
