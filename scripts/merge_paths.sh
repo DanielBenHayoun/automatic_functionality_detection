5fd6589eb12cfb6ad2242289048feac83bec7aae
daniel.bh38 <daniel@daniel.bh38-VirtualBolx>
2020-08-23 17:45:08 +0300

    new data set

--- scripts/merge_paths.sh
+++ scripts/merge_paths.sh
@@ -8,6 +8,7 @@
 # merge paths to datasets ( train test and validation)
 # NOTE: output is in the same folder as SOURCE_PATH
 
-SOURCE_PATH=~/automatic_functionality_detection/cppminer_res
+# SOURCE_PATH=~/automatic_functionality_detection/cppminer_res
+SOURCE_PATH=~/automatic_functionality_detection/data
 PROJECT_FOLDER=~/automatic_functionality_detection/
 python3  ${PROJECT_FOLDER}/cppminer/src/merge.py  $SOURCE_PATH
