8fcfb37fcc8263da69285ee877710b50fe77a4f8
danielbenhayoun <danielben@rishon.csf.technion.ac.il>
2020-08-24 17:07:03 +0300

    new script for starting new train

--- functions_filter.py
+++ functions_filter.py
@@ -1,11 +1,13 @@
 import glob
+import sys
 
-files= glob.glob("cppminer_res/*.c2s")
-limit = 1000 #the limit for path length, change it to the size of the path you want to filter
-with open("data/filterd_functions.c2s",'w') as output:
+limit = sys.argv[1] #the limit for path length, change it to the size of the path you want to filter
+print("limit is " +limit)
+files= glob.glob("../cppminer_res/*.c2s")
+with open("../data/filterd_functions.c2s",'w') as output:
 	for idx,file in enumerate(files):
 		with open(file,'r') as fp:
 			for line in fp:
 				count = line.count('|')+1
-				if count<limit:
+				if count<int(limit):
 					output.write(line)
