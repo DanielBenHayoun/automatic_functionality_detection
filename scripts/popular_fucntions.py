61d690e742ba4b79a28f98ab595fa2079dee10b7
Eial Perlsteyn <eialpr@gmail.com>
2020-09-08 15:45:37 +0300

    replaced popular_functions.py with prepreprocess.py, and also added new dataset

--- scripts/popular_fucntions.py
+++ scripts/popular_fucntions.py
@@ -1,6 +1,7 @@
 import sys 
 import os
 import glob
+import math
 
 #finding the name of the fucntion	
 def func_name_extractor(function):
@@ -11,50 +12,93 @@ def func_name_extractor(function):
 	return function_name
 	
 
-def main(files , output_folder):
+#input:  -dict of the popular functions with subtokens as key
+#        -the folder to output
+#output: -train,test,val files in the output_folder
+def split_data(popular_mapper,output_folder):
+
+		
+	trainfile = open(output_folder+"/dataset.train.c2s", "w")
+	testfile  = open(output_folder+"/dataset.test.c2s", "w")
+	valfile   = open(output_folder+"/dataset.val.c2s", "w")
+	
+	
+	
+	used_funcs = []
+	for subtoken,functions in popular_mapper.items():
+		valid_funcs = []
+		for function in functions:
+			if function in used_funcs:
+				continue
+			used_funcs.append(function)
+			valid_funcs.append(function)
+			
+		valid_funcs_length = len(valid_funcs)
+		i = 0
+		#print("len is ",valid_funcs_length)
+		
+		
+		
+		if valid_funcs_length>=3:
+			while i<math.floor(0.7*valid_funcs_length):
+				#print("train file write")
+				#print("0.7*valid_funcs_length is:",math.floor(0.7*valid_funcs_length))
+				#print("i is:",i)
+				trainfile.write(valid_funcs[i])
+				i+=1
+			while i<math.floor(0.85*valid_funcs_length):
+				#print("val file write")
+				valfile.write(valid_funcs[i])
+				i+=1
+			while i<valid_funcs_length:
+				#print("test file write")
+				testfile.write(valid_funcs[i])
+				i+=1
+		else:
+			while i<valid_funcs_length:
+				trainfile.write(valid_funcs[i])
+				i+=1
+				
+	trainfile.close()
+	testfile.close()
+	valfile.close()
+
+#input :-files to scan	   
+#output:-dict with subtoken as key,and the items are a list of fucntions(with function name and paths) corresponding to that ket
+def popular_functions(files):
 	mapper = dict()
-	popular_funcs = set()
-
+	
 	for file in files :
-
+		max_subtoken =0
 		# with open("../data/filterd_functions.c2s",'r') as fp:
 		with open(file,'r') as fp:
 			for function in fp:
 				function_name = func_name_extractor(function)
 				subtokens=function_name.split("|")
-				#print(subtokens)
-				#print("............................................")
+				if len(subtokens) > max_subtoken:
+					max_subtoken=len(subtokens)
 				#making a dictionary that the key is a name of a subtoken, and the value is a list of the fucntions name with the subtoken is their name
 				for label in subtokens:
 					if label not in mapper:
 						mapper[label] = []
-					mapper[label].append(function_name)
+					mapper[label].append(function)
+					
 				#end of the making of the dictionary
 				
 			#finding the popular functions names
-	for partial in map(lambda x: x[1], filter(lambda x: len(x[1]) >= 3, mapper.items())):
-		popular_funcs.update(partial)
-	popular_funcs = list(popular_funcs)
-#end of finding the popular functions names
-			#print("++++++++++++++++++++++++++++++++++++++++++++++++++")
-	# print(popular_funcs)
-	print(len(popular_funcs))
-		
-	#writing the popular functions into the output file
-	
-	with open(output_folder +"filterd_and_popular_functions.c2s",'w') as output:
-		print("files")
-		print(files)
-		print(len(files))
-		for file in files :
-			print(file)
-			with open(file,'r') as fp:
-				for function in fp:
-					function_name = func_name_extractor(function)
-					if function_name in popular_funcs:
-						output.write(function)
-						# print(function_name)
 			
+	print("max subtokens is: ",max_subtoken)
+	popular_mapper=dict(filter(lambda x: len(x[1]) >= 3, mapper.items()))
+	
+	return popular_mapper
+	
+	
+def main(files , output_folder):
+	
+	popular_mapper = popular_functions(files)
+	
+	split_data(popular_mapper,output_folder)
+	
 if __name__ == '__main__':
 	input_folder = sys.argv[1]
 	output_folder= sys.argv[2]
